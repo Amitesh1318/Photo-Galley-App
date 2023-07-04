42dba9a4341434fa3539908bb6d5599ee78bf2c2
Amitesh1318 <sangrah007@gmail.com>
2023-07-04 23:07:52 +0530

    final

---
+++ src/content/data.js
@@ -0,0 +1,65 @@
+export const data = [
+  {
+    "name": "Photo 1",
+    "url": "/images/backiee-96330.jpg",
+    "description": "Photo 1 description",
+    "date": "2020-01-01",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 2",
+    "url": "/images/ev3797p9ivh61.png",
+    "description": "Photo 2 description",
+    "date": "2020-01-02",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 3",
+    "url": "/images/god-of-war-ragnarok-46.jpg",
+    "description": "Photo 3 description",
+    "date": "2020-01-03",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 4",
+    "url": "/images/HD wallpaper_ Anime, One Piece, Monkey D_ Luffy.jpeg",
+    "description": "Photo 4 description",
+    "date": "2020-01-04",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Photo 5",
+    "url": "/images/HD wallpaper_ One Piece, Monkey D_ Luffy.jpeg",
+    "description": "Photo 5 description",
+    "date": "2020-01-05",
+    "type": "image/jpeg",
+  },
+  {
+    "name": "Photo 6",
+    "url": "/images/kimetsu-no-yaiba-wallpaper-20083114033610-scaled.jpg",
+    "description": "Photo 6 description",
+    "date": "2020-01-06",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 7",
+    "url": "/images/n-i-x-e-u-sand-01.jpg",
+    "description": "Photo 7 description",
+    "date": "2020-01-07",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 8",
+    "url": "/images/wallpaperflare.com_wallpaper (1).jpg",
+    "description": "Photo 8 description",
+    "date": "2020-01-08",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 9",
+    "url": "/images/wp5956614-anime-girl-with-mask-wallpapers.jpg",
+    "description": "Photo 9 description",
+    "date": "2020-01-09",
+    "type": "image/jpg",
+  }
+]
